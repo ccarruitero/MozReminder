2c9385f5130f87dc826f63283cb97ca9885fe79c
Cesar Carruitero <cesar@mozilla.pe>
2012-01-29 18:59:22 -0500

    resolve errors

--- MozReminder/items.py
+++ MozReminder/items.py
@@ -1,12 +1,7 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/topics/items.html
-
 from scrapy.item import Item, Field
 
 class MozreminderItem(Item):
+
     tarea = Field()
     link_tarea = Field()
     limite = Field()
-
--- MozReminder/spiders/mozreminder.py
+++ MozReminder/spiders/mozreminder.py
@@ -6,18 +6,19 @@ from MozReminder.items import MozreminderItem
 class TareaSpider(BaseSpider):
   name = "tareas"
   allowed_domains = ["mozilla-hispano.org"]
-  start_url = [
+  start_urls = [
 	"https://www.mozilla-hispano.org/documentacion/Tareas"
   ]
 
   def parse(self, response):
-	hxs = HtmlXPathSelector(response)
-        sites = hxs.select('//table/tr')
-        for site in sites:
-                item = MozreminderItem()
-                item['tarea'] = site.select('td/a/text()').extract()
-                item['link_tarea'] = site.select('td/a/@href').extract()
-                item['limite'] = site.select('td[6]').extract()
-                items.append(item)
-        return items
+      hxs = HtmlXPathSelector(response)
+      sites = hxs.select('//table/tr')
+      items = []
+      for site in sites:
+          item = MozreminderItem()
+          item['tarea'] = site.select('td/a/text()').extract()
+          item['link_tarea'] = site.select('td/a/@href').extract()
+          item['limite'] = site.select('td[6]').extract()
+          items.append(item)
+      return items
 
