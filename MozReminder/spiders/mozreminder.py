80033f89dcc5eb3d99a95680279d2327c687582c
Cesar Carruitero <cesar@mozilla.pe>
2012-01-30 00:25:05 -0500

    some changes (but with error)

--- MozReminder/spiders/mozreminder.py
+++ MozReminder/spiders/mozreminder.py
@@ -1,6 +1,6 @@
 from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
-
+from scrapy.http import Request
 from MozReminder.items import MozreminderItem
 
 class TareaSpider(BaseSpider):
@@ -17,22 +17,22 @@ class TareaSpider(BaseSpider):
       for site in sites:
           item = MozreminderItem()
           item['tarea'] = site.select('td/a/text()').extract()
-          'link_tarea' = site.select('td/a/@href').extract()
+          link_tarea = site.select('td/a/@href').extract()
           item['limite'] = site.select('td[6]/text()').extract()
 	  items.append(item)
-      for url in 'link_tarea':
-	  yield Request(url, callback=self.parse_links)
+      for url in link_tarea:
+	  yield Request("https://www.mozilla-hispano.org"+url, callback=self.parse_links)
   def parse_links(self, response):
       hxs = HtmlXPathSelector(response)
       links = hxs.select('//div[@id="bodyContent"]/ul/li/a')
-      for link in links
+      for link in links:
 	  item['responsable'] = link.select('text()').extract()
-	  'link_user' = link.select('@href').extract()
-      for url in 'link_user':
-	  yield Request(url, callback=self.parse_user)
+	  link_user = link.select('@href').extract()
+      for url in link_user:
+	  yield Request("https://www.mozilla-hispano.org"+url, callback=self.parse_user)
   def parse_user(self, response):
       hxs = HtmlXPathSelector(response)
       users = hxs.select('//table/tr[10]/td[2]')
-      for user in users
+      for user in users:
 	  item['mail'] = user.select('text()').extract()
       return items
